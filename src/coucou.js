f14990346d7f904bcd9c0dcb90ca554b09cd702e
arthur@aclee.fr <arthur@aclee.fr>
2023-04-10 14:24:28 +0200

    bien joue champion

---
+++ src/coucou.js
@@ -0,0 +1,19 @@
+let n = 0;
+let k = 10;
+let res = 0;
+for (; n < k; n++) {
+    res += n + k;
+    k--;
+}
+console.log(res);
+
+let a;
+for (a = 6; a >= 3; a--) {
+    console.log("moments");
+}
+
+for(let b = 10;b >= 0;b--){
+  console.log(b);
+}
+
+//Your task is to insert a paragraph element with the text The answer is 42 into the heading. Change the HTML content using a single line of code.
